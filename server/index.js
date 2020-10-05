0b23571a1647231bfb9a4a2f934845907bf683f7
Kousthubha <krishnakousthubha@gmail.com>
2020-10-05 09:41:51 +0530

    Initial Commit with Project Setup

---
+++ server/index.js
@@ -0,0 +1,43 @@
+// Required Imports
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// Initialise application 
+const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(express.json());
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError) return res.status(400).send(JSON.stringify({
+        error: "Invalid JSON"
+    }))
+    console.error(err);
+    res.status(500).send();
+});
+
+// Routes
+app.get('/api', (req, res) => {
+    res.send("CDC API");
+});
+
+
+// Import Routes
+//  const usersRoutes = require('./routes/usersRoutes');
+// application.use("/api/users", usersRoutes)
+
+// Handle Production
+if(process.env.NODE_ENV=='production'){
+    // Static folder
+    app.use(express.static(__dirname+'/public/'));
+
+    // Handle SPA
+    app.get(/.*/,(req,res)=>res.sendFile(__dirname+'/public/index.html'));
+}
+// Starting Server to listen 
+const port = process.env.PORT || 3080;
+app.listen(port, () => {
+    console.log('Server started at port: ' + port);
+});
---
+++ server/public/precache-manifest.8cad101f6c46e203c2a0bdd35e5fc78d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da2017e7cc799e53ad3",
+    "url": "/css/app.341cc59d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c239bd31d73692a01a5c44e53f075502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7a40f846605c76439a",
+    "url": "/js/about.fc9ec4c9.js"
+  },
+  {
+    "revision": "6da2017e7cc799e53ad3",
+    "url": "/js/app.2b818aea.js"
+  },
+  {
+    "revision": "edb8825316e62a7277ae",
+    "url": "/js/chunk-vendors.2cb17a40.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
