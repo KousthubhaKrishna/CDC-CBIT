b6c8e07fe318e90d7046d85595719a58e32c0db5
Kousthubha Krishna CH <kousthu@amazon.com>
2021-04-25 18:38:46 +0530

    Modified authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -134,7 +134,7 @@ router.get("/forgot_password/:user_email", async (req, res) => {
         });
         if (account == null)
             return res.status(401).json({ message: "Account does not exists for given email" });
-    
+
         reset_token = crypto.randomBytes(20).toString('hex');
         const existing_token = await Token.findOne({
             user_email: user_email,
@@ -142,28 +142,29 @@ router.get("/forgot_password/:user_email", async (req, res) => {
         if (existing_token != null) {
 
             const updateToken = await Token.updateOne(
-                {user_email : user_email},
-                {$set: {
-                    value: reset_token,
-                    createdAt: new Date(),
-                }
-             });
-
-             console.log(updateToken);
-            
-        } else {
-                const insObj = new Token( {
-                    user_email: user_email,
-                    value: reset_token,
-                    createdAt: new Date(),    
+                { user_email: user_email },
+                {
+                    $set: {
+                        value: reset_token,
+                        createdAt: new Date(),
+                    }
                 });
-                const insertToken = await insObj.save();
 
-                console.log(insertToken);
-        } 
-        
+            console.log(updateToken);
+
+        } else {
+            const insObj = new Token({
+                user_email: user_email,
+                value: reset_token,
+                createdAt: new Date(),
+            });
+            const insertToken = await insObj.save();
+
+            console.log(insertToken);
+        }
+
         const link = `${"cdc-cbit"}/passwordReset?token=${reset_token}&id=${user_email}`;
-        const responseMessage = await SendEmail(user_email,"Password Reset Request",{email: user_email,link: link,},"./template/requestResetPassword.handlebars");
+        const responseMessage = await SendEmail(user_email, "Password Reset Request", { email: user_email, link: link, }, "./template/requestResetPassword.handlebars");
         res.json({ message: responseMessage.message });
     } catch (err) {
 
@@ -173,29 +174,30 @@ router.get("/forgot_password/:user_email", async (req, res) => {
 
 router.post("/forgot_password_reset/", async (req, res) => {
     // body has token email and new password
-    try{
+    try {
         const password_token = req.body.token;
-        const user_email = req.body.user_email;
+        const user_email = req.body.email;
 
         const salt = await bcrypt.genSalt();
-        const new_password = await bcrypt.hash(req.body.new_password,salt);
+        const new_password = await bcrypt.hash(req.body.newPassword, salt);
 
-        const obj = await Token.findOne({user_email: user_email});
+        const obj = await Token.findOne({ user_email: user_email });
 
-        if(obj.value == password_token){
+        if (obj.value == password_token) {
             const updateAUth = await Auth.updateOne(
-                {user_email: user_email},
-                {$set: {
-                    password: new_password
-                },
-            });
+                { user_email: user_email },
+                {
+                    $set: {
+                        password: new_password
+                    },
+                });
             res.status(200).send({ message: "Password change Successful" });
         }
-        else{
-            res.status(403).json({message: "Token not matched"});
+        else {
+            res.status(403).json({ message: "Token not matched" });
         }
     }
-    catch(err){
+    catch (err) {
         res.status(400).json({ message: err.message });
     }
 });
@@ -207,36 +209,36 @@ router.post("/forgot_password_reset/", async (req, res) => {
 
 router.post("/password_reset/", async (req, res) => {
     //body has old password,new password
-    try{
+    try {
         const token = req.cookies.jwt;
-        if(token == null){
+        if (token == null) {
             return res.json({ message: "Not Authorised" });
         }
 
         var decoded = jwt.decode(token);
         const old_password_object = await Auth.findById(decoded._id);
-        old_password_db = old_password_object.password;        
+        old_password_db = old_password_object.password;
 
         const salt = await bcrypt.genSalt();
-        const new_password = await bcrypt.hash(req.body.new_password,salt);
+        const new_password = await bcrypt.hash(req.body.new_password, salt);
 
         const validPass = await bcrypt.compare(
             req.body.old_password,
             old_password_db
         );
-        
-        if(validPass){
-            const updateAUth = await Auth.findByIdAndUpdate( decoded._id, 
-                { password: new_password}
-                );
+
+        if (validPass) {
+            const updateAUth = await Auth.findByIdAndUpdate(decoded._id,
+                { password: new_password }
+            );
             res.status(200).send({ message: "Password change Successful" });
         }
-        else{
+        else {
             res.json({ message: "Old Password is incorrect " });
         }
     }
-    catch(err){
-        res.json({message: err.message});
+    catch (err) {
+        res.json({ message: err.message });
     }
 });
-                
+
